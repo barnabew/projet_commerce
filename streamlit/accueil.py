e17458d3a1d2d677c7a4d8bb07480d317e4713e8
barnabew <barnabe.willenbucher@outlook.fr>
2025-11-15 14:01:52 +0100

    Update accueil.py

--- streamlit/accueil.py
+++ streamlit/accueil.py
@@ -1,1 +1,11 @@
 
+st.set_page_config(
+    page_title="Analyse et Prédiction du Churn Client",
+    page_icon="📉",
+    layout="wide"
+)
+
+st.title("Projet : Analyse businness e-commerce")
+st.markdown(intro)
+
+st.sidebar.success("Choisissez une page ci-dessus pour commencer.")
