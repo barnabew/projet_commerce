24cee8308d943c203972bb2b7d4286298e74d2b4
barnabew <barnabe.willenbucher@outlook.fr>
2025-11-15 14:07:24 +0100

    Update 1_resume.py

--- streamlit/pages/1_resume.py
+++ streamlit/pages/1_resume.py
@@ -8,3 +8,27 @@ WHERE o.order_status IN ("delivered", "shipped", "invoiced");
 """
 
 revenue_total = run_query(query)
+
+
+query="""
+SELECT 
+    (SELECT SUM(price + freight_value) FROM olist_order_items_dataset) /
+    (SELECT COUNT(DISTINCT order_id) FROM olist_orders_dataset) AS Panier_Moyen;
+"""
+
+panier_moyen = run_query(query)
+
+query="""
+SELECT 
+    ROUND(AVG(
+        JULIANDAY(order_delivered_customer_date) 
+        - JULIANDAY(order_purchase_timestamp)
+    ), 2) AS avg_delivery_time_days
+FROM clean_orders
+WHERE order_status = "delivered"
+  AND order_delivered_customer_date IS NOT NULL
+  AND order_purchase_timestamp IS NOT NULL;
+"""
+
+delai_moyen = run_query(query)
+pd.read_sql(query,conn)
