7cea88caf6d7cfa55a25e0c84d888b261bbb8f2d
barnabew <barnabe.willenbucher@outlook.fr>
2025-11-17 11:55:43 +0100

    Update textes.py

--- streamlit/textes.py
+++ streamlit/textes.py
@@ -1,2 +1,12 @@
 intro="""
 """
+
+texte_intro_produits = """
+Cette page analyse les performances produits :  
+- **catégories les plus rentables**  
+- **produits les plus vendus**  
+- **prix moyens**  
+- **satisfaction client**  
+- **délais de livraison**  
+- **analyse interactive par catégorie**  
+"""
