c3c9340f6dcd574c4c79cbc1824c66f6898a76ed
barnabew <barnabe.willenbucher@outlook.fr>
2025-11-19 18:04:23 +0100

    Change import from data to utils for functions

--- streamlit/pages/4_clients.py
+++ streamlit/pages/4_clients.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
-from data import load_table, run_query
+from utils import load_table, run_query
 
 st.title("🧍 Analyse Clients")
 
